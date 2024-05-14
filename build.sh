6dc9b6f581bcb6a8353bce23abab248a355bfce4
William Hazard <105560469+williamthazard@users.noreply.github.com>
2024-05-13 23:00:11 -0400

    first entry
    add initial entry 240513

---
+++ build.sh
@@ -0,0 +1,88 @@
+# echo ">> root .md to .html"
+# function htmlify() {
+#   for file in *.md ; do
+#     date=$(date -r ${file} +%D)
+#     file=${file%.*}
+#     echo "building $file"
+#     target=${file}.html
+#     cat $1 > ${target}
+#     cmark --unsafe ${file}.md >> ${target}
+#     cat $2 >> ${target}
+#     sed -i '' -e 's#DATE#'$date'#g' ${target}
+#     echo "$file built"
+#   done
+# }
+# htmlify "head.htm_" "foot.htm_"
+# for subdir in ./*/ ; do
+#   cd $subdir
+#   htmlify "../head.htm_" "../foot.htm_"
+#   cd ..
+# done
+
+echo ">> build rss"
+index="index"
+
+cat head.htm_ > ${index}.html
+cat start_rss.xml_ > rss.xml
+
+n=1
+
+cd days
+
+marks=(*.md)
+min=1
+max=$(( ${#marks[@]} ))
+while [[ min -lt max ]] ; do
+    # Swap current first and last elements
+    x="${marks[$min]}"
+    marks[$min]="${marks[$max]}"
+    marks[$max]="$x"
+
+    # Move closer
+    (( min++, max-- ))
+done
+
+for file in $marks ; do
+  # convert md to html
+  date=$(date -r ${file} +%D)
+  file=${file%.*}
+  name=${file#*/}
+  folder=$(basename $(pwd))
+  target=${file}.html
+  cat ../head.htm_ > ${target}
+  echo "<p>${name}</p>" >> ${target}
+  cmark --unsafe ${file}.md >> ${target}
+  cat ../log-foot.htm_ >> ${target}
+  echo $name
+
+  # paginate
+  if [[ $((n % 10)) == 0 ]]; then
+    echo "<br/><p><a href=../${index}n.html>[further]</a></p>" >> ../${index}.html
+    cat ../log-foot.htm_ >> ../${index}.html
+    index=$index"n"
+    cat ../head.htm_ > ../${index}.html
+  fi
+  ((n=n+1))
+
+  # append to index
+  echo "<p><a href=days/${target}>${name}</a></p>" >> ../${index}.html
+  cmark --unsafe ${file}.md >> ../${index}.html
+  echo "<br/>" >> ../${index}.html
+
+  # append to rss
+  echo "<item>" >> ../rss.xml
+  echo "<title>daily reflections / ${name}</title>" >> ../rss.xml
+  echo "<link>https://williamthazard.github.io/daily-reflections/${folder}/${name}.html</link>" >> ../rss.xml
+  echo "<guid>https://williamthazard.github.io/daily-reflections/${folder}/${name}.html</guid>" >> ../rss.xml
+  echo "<description><![CDATA[" >> ../rss.xml
+  cmark --unsafe ${file}.md >> ../rss.xml
+  echo "]]></description>" >> ../rss.xml
+  date=$(date -r ${file}.md "+%a, %d %b %Y 11:11:11 EST")
+  echo "<pubDate>$date</pubDate>" >> ../rss.xml 
+  echo "</item>" >> ../rss.xml
+done
+
+cat ../log-foot.htm_ >> ../${index}.html
+date=$(date -r ../${index}.html +%D)
+sed -i '' -e 's#DATE#'$date'#g' ../${index}.html
+cat ../end_rss.xml_ >> ../rss.xml
