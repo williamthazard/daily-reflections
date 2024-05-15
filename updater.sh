6e2d080c98215951038bc5f45d12a522200a26e0
William Hazard <105560469+williamthazard@users.noreply.github.com>
2024-05-15 04:45:24 -0400

    daily update

--- updater.sh
+++ updater.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo ">> updating daily reflection"
 
-# git add .
-# git commit -m 'daily update'
-# git push -u origin
+git add .
+git commit -m 'daily update'
+git push -u origin
 
 echo ">> daily reflection updated"
