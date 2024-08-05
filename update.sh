71a50aba5ffd58f5a84aceda7b273b1c434cd6c6
William Hazard <105560469+williamthazard@users.noreply.github.com>
2024-08-05 16:52:32 -0400

    daily update

--- update.sh
+++ update.sh
@@ -8,6 +8,7 @@ rm -f .git/index.lock
 echo ">> worktree cleared"
 
 echo ">> updating daily reflection"
+git config --global --add safe.directory /Users/spencerkingmangraham/Desktop/other/daily-reflections
 git add .
 git commit -m 'daily update'
 git push -u origin
