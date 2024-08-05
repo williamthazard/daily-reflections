dbd68a268dbc46e27819a1933967af4d9819e63c
William Hazard <105560469+williamthazard@users.noreply.github.com>
2024-08-04 20:48:35 -0400

    daily update

--- update.sh
+++ update.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 zsh ./build.sh
 
-echo ">> updating daily reflection"
+echo ">> clearing worktree"
+rm -f .git/index.lock
+echo ">> worktree cleared"
 
+echo ">> updating daily reflection"
 git add .
 git commit -m 'daily update'
 git push -u origin
-
 echo ">> daily reflection updated"
