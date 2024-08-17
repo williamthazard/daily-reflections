3f58a29320f841ab3474491dac461d6e23e7ee93
williamthazard <105560469+williamthazard@users.noreply.github.com>
2024-08-17 15:58:03 -0400

    update

--- update.sh
+++ update.sh
@@ -7,9 +7,6 @@ echo ">> clearing worktree"
 rm -f .git/index.lock
 echo ">> worktree cleared"
 
-git config --global user.name "williamthazard"
-git config --global user.email "105560469+williamthazard@users.noreply.github.com"
-
 echo ">> updating daily reflection"
 git add .
 git commit -m 'update'
